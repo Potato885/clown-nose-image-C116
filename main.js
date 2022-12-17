c40161adec644ee4b5b22c9ac24200fe9c3cc3f9
Potato885 <75574117+Potato885@users.noreply.github.com>
2022-12-17 11:35:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX = 0;
+noseY = 0;
+
+
+
+function preload() {
+    clown_nose = loadImage('https://i.postimg.cc/x8wWLKNL/580b57fbd9996e24bc43bed5.png');
+}
+
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+
+function modelLoaded() {
+    console.log('poseNet model is loaded');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log('nose x = ' + noseX);
+        console.log('nose y = ' + noseY);
+    }
+
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX, noseY, 25, 25);
+}
+
+
+function take_snapshot() {
+    save("clown-nose-image-filter.png");
+}
